4be57cc7d3415058b993183a4dc19e86be36a3f2
Swati Mandal <92773616+miss-audiovideo@users.noreply.github.com>
2021-10-26 13:16:32 +0530

    done

--- src/Engineering/Banner.js
+++ src/Engineering/Banner.js
@@ -1,7 +1,7 @@
 import React from "react";
-import toolone from "../Engineering/img/banner/toolone.jpg";
-import tooltwo from "../Engineering/img/banner/tooltwo.jpg";
-import toolthree from "../Engineering/img/banner/toolthree.jpg";
+import toolone from "../Engineering/img/banner/d.jpg";
+import tooltwo from "../Engineering/img/banner/g.jpg";
+import toolthree from "../Engineering/img/banner/p.jpg";
 import Slider from "react-animated-slider";
 import "react-animated-slider/build/horizontal.css";
 import "./Banner.css";
@@ -12,139 +12,104 @@ import "./Banner.css";
 //   { title: "Second item", description: "Lorem ipsum", img: { banner } },
 // ];
 
-const slides = [
-  {
-    title: "Rotating Equipment",
-    description:
-      "Our expertise in Rotating Equipment Engineering helps you achieve peak performance.",
-    button: "Explore More",
-    image: toolone,
-    // path: "/rotatingequipment",
-  },
-  {
-    title: "Mechanical Engineering",
-    description:
-      "Our expertise in mechanical engineering helps you manage risks and reduce costs.",
-    button: "Explore More",
-    image: tooltwo,
-    // path: "/mechanicalengineering",
-  },
-  {
-    title: "Digitalization",
-    description: "Our augumented technologies helps Industry 4.0 move forward.",
-    button: "Explore More",
-    image: toolthree,
-    // path: "/digitalization",
-  },
-  {
-    title: "Plant Services",
-    description:
-      "Our best maintenance practices helps you achieve top quartile performance.",
-    button: "Explore More",
-    image: tooltwo,
-    // path: "/services",
-  },
-];
 
-const Banner = () => {
-  return (
-    <div>
-      {/* <Slider className="slider-wrapper" autoplay={3500}>
-        {slides.map((item, index) => (
-          <div
-            key={index}
-            className="slider-content"
-            style={{
-              background: `url('${item.image}') no-repeat center center`,
-            }}
-          >
-            <div className="inner">
-              <p className="title  is-size-1-desktop is-size-3-mobile has-text-weight-bold ">
-                {item.title}
-              </p>
 
-              <p className="subtitle  is-size-5-desktop is-size-5-mobile has-text-weight-medium">
-                {item.description}
-              </p>
-              <NavLink to={item.path} exact>
-              <button class="button is-rounded is-primary">
-                {item.button}
-              </button>
-              </NavLink>
-            </div>
-          </div>
-        ))}
-      </Slider> */}
-      <Slider className="slider-wrapper" autoplay={2500}>
-        {slides.map((item, index) => (
-          <div
-            key={index}
-            className="slider-content"
-            style={{
-              background: `url('${item.image} ')`,
-              width: `100%`,
-              height: `400px`,
-              opacity: `0.7`,
-            }}
-          >
-            <div
-              className="inner"
-              style={{
-                display: `flex`,
-                flexDirection: `column`,
-                alignItems: `center`,
-                justifyContent: `center`,
-                color: `black`,
-              }}
-            >
-              <h1
-                className="title animate__animated animate__fadeInDown animate__delay-2s"
-                style={{
-                  color: `orange`,
-                  fontSize: `38px !important`,
-                  background: `black`,
-                  padding: `15px`,
-                  borderRadius: `10px`,
-                }}
-              >
-                {item.title}
-              </h1>
-              <br />
-              <p
-                className="subtitle animate__animated animate__fadeInUp animate__slower animate__delay-2s"
-                style={{
-                  color: `red`,
-                  fontSize: `28px !important`,
-                  fontWeight: `bold`,
-                  background: `black`,
-                  padding: `10px`,
-                  borderRadius: `5px`,
-                }}
-              >
-                {item.description}
-              </p>
+ class Banner extends React.Component {
+  render() {
+      return (
+          <div>
 
-              <button
-                class="button"
-                style={{
-                  color: `white`,
-                  fontSize: `28px !important`,
-                  fontWeight: `bold`,
-                  background: `blue`,
-                  // padding: `10px`,
-                  borderRadius: `5px`,
-                  textDecoration: `none`,
-                }}
-              >
-                {item.button}
-              </button>
-            </div>
-          </div>
-        ))}
-      </Slider>
-      <style jsx>{``}</style>
+<div class="wrapper">
+  <input checked type="radio" name="slider" id="slide1" />
+  <input type="radio" name="slider" id="slide2" />
+  <input type="radio" name="slider" id="slide3" />
+  <input type="radio" name="slider" id="slide4" />
+  <input type="radio" name="slider" id="slide5" />
+
+  <div class="slider-wrapper" style={ {backgroundColor:"black",color: "#fff",
+    opacity: .5}}>
+    <div class="inner">
+      <article>
+        <div class="info top-left" >
+          <h1 style={{color:"orange",marginTop:"390px",opacity:"1"}}>We help Industries&nbsp;<br/><br/>
+<span style={{color:"white", fontSize:"38px"}}>Innovate and Grow</span></h1>
+<p style={{marginRight:"80px",color:"white",marginLeft:"-10px"}}>
+  will be always aware of all business and financial news and stay informed<br/> with
+investment tips, market predictions, business advice and guides.</p>
+</div> <img src="https://farm9.staticflickr.com/8059/28286750501_dcc27b1332_h_d.jpg" />
+      </article>
+
+      <article>
+      <div class="info top-left" >
+          <h1 style={{color:"orange",marginTop:"390px",opacity:"1"}}>We help Industries&nbsp;<br/><br/>
+<span style={{color:"white", fontSize:"38px"}}>Innovate and Grow</span></h1>
+<p style={{marginRight:"80px",color:"white",marginLeft:"-10px"}}>
+  will be always aware of all business and financial news and stay informed<br/> with
+investment tips, market predictions, business advice and guides.</p>
+</div>
+        <img src="https://farm6.staticflickr.com/5812/23394215774_b76cd33a87_h_d.jpg" />
+      </article>
+
+      <article>
+      <div class="info top-left" >
+          <h1 style={{color:"orange",marginTop:"390px",opacity:"1"}}>We help Industries&nbsp;<br/><br/>
+<span style={{color:"white", fontSize:"38px"}}>Innovate and Grow</span></h1>
+<p style={{marginRight:"80px",color:"white",marginLeft:"-10px"}}>
+  will be always aware of all business and financial news and stay informed<br/> with
+investment tips, market predictions, business advice and guides.</p>
+</div>
+        <img src="https://farm8.staticflickr.com/7455/27879053992_ef3f41c4a0_h_d.jpg" />
+      </article>
+
+      <article>
+      <div class="info top-left" >
+          <h1 style={{color:"orange",marginTop:"390px",opacity:"1"}}>We help Industries&nbsp;<br/><br/>
+<span style={{color:"white", fontSize:"38px"}}>Innovate and Grow</span></h1>
+<p style={{marginRight:"80px",color:"white",marginLeft:"-10px"}}>
+  will be always aware of all business and financial news and stay informed<br/> with
+investment tips, market predictions, business advice and guides.</p>
+</div>
+        <img src="https://farm8.staticflickr.com/7367/27980898905_72d106e501_h_d.jpg" />
+      </article>
+
+      <article>
+      <div class="info top-left" >
+          <h1 style={{color:"orange",marginTop:"390px",opacity:"1"}}>We help Industries&nbsp;<br/><br/>
+<span style={{color:"white", fontSize:"38px"}}>Innovate and Grow</span></h1>
+<p style={{marginRight:"80px",color:"white",marginLeft:"-10px"}}>
+  will be always aware of all business and financial news and stay informed<br/> with
+investment tips, market predictions, business advice and guides.</p>
+</div>
+        <img src="https://farm8.staticflickr.com/7356/27980899895_9b6c394fec_h_d.jpg" />
+      </article>
     </div>
-  );
-};
+ 
+  </div>
+ 
+
+  <div class="slider-prev-next-control">
+    <label for="slide1"></label>
+    <label for="slide2"></label>
+    <label for="slide3"></label>
+    <label for="slide4"></label>
+    <label for="slide5"></label>
+  </div>
+  
+
+  <div class="slider-dot-control">
+    <label for="slide1"></label>
+    <label for="slide2"></label>
+    <label for="slide3"></label>
+    <label for="slide4"></label>
+    <label for="slide5"></label>
+  </div>
+
+</div>  
+          </div>
+
+      );
+  }
+}
+
 
 export default Banner;
